8c88a47233fa51217b6492abc79dacaf5b86bacf
Peeyush Kant Misra <46857249+misrapk@users.noreply.github.com>
2023-02-22 15:19:42 +0530

    Update ipFinder.py

--- ipFinder.py
+++ ipFinder.py
@@ -2,8 +2,15 @@ import socket
 import time
 
 
-dnsName = "cognizant.com" #enter server name
+dnsName = input("ENTER SERVER: ") #enter server name
+ipadr = socket.gethostbyname(dnsName)
+
 while True:
-    ip = socket.gethostbyname(dnsName)
-    print(ip)
+    newIp = socket.gethostbyname(dnsName)
+    if(newIp != ipadr):
+        ipadr = newIp
+        print(f"IP CHANGED! New IP: {ipadr}")
+    print(f"IP: {ipadr}")
+        
     time.sleep(900)   #15 minutes(15*60 = 900s)
+
