a95a6ec8d745b55f8664d92f032876e2efec67ab
Peeyush Kant Misra <46857249+misrapk@users.noreply.github.com>
2023-02-22 15:42:39 +0530

    added the notification functionality
    it will give a desktop notification whenever IP Changes

--- ipFinder.py
+++ ipFinder.py
@@ -1,16 +1,23 @@
 import socket
 import time
-
+from plyer import notification  
 
 dnsName = input("ENTER SERVER: ") #enter server name
-ipadr = socket.gethostbyname(dnsName)
+ipadr = socket.gethostbyname(dnsName)       #save the default ip address
 
 while True:
-    newIp = socket.gethostbyname(dnsName)
+    newIp = socket.gethostbyname(dnsName) #check for new ip everytime
     if(newIp != ipadr):
         ipadr = newIp
         print(f"IP CHANGED! New IP: {ipadr}")
-    print(f"IP: {ipadr}")
+        
+        #give the desktop notification
+        notification.notify(
+            title='IP Address Changed',
+            message=f'New IP: {ipadr}',
+            timeout=10
+        )
+    else:
+        print(f"IP: {ipadr}")
         
     time.sleep(900)   #15 minutes(15*60 = 900s)
-
